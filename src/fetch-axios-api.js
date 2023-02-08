6642263eff78006eb52b4e60e116bc5a301b06d3
Volodymyr <nebesnyivr@gmail.com>
2023-02-08 11:42:14 +0200

    errorme

--- src/fetch-axios-api.js
+++ src/fetch-axios-api.js
@@ -1,23 +1,26 @@
+// const axios = require('axios');
+const API_KEY = '33395232-2c7298051ec43baaf859d6c71';
+const BASE_URL = 'https://pixabay.com/api/';
 
 export default class SeacrchApiService {
     constructor(){
         this.searchQuery = '';
         this.page = 1;
+        this.totalHits =0;
     }
-
+// fetch()
     fetchHits(){
-    const API_KEY = '33395232-2c7298051ec43baaf859d6c71';
-    const BASE_URL = 'https://pixabay.com/api/';
+        const url =`${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`;
 
-        return fetch(`${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`)
-        .then(res => res.json())
-        .then(data =>{
+        return fetch(url)
+        .then(response => response.json())
+        .then(({hits}) =>{
             this.incrementPage();
 
-            return data.hits;
-        });
+            return hits;
+        })
     }
-
+    
     incrementPage(){
         this.page +=1;
     }
--- src/index.js
+++ src/index.js
@@ -2,35 +2,54 @@ import SeacrchApiService from "./fetch-axios-api";
 import renderImgMarcup from "./renderImgMarcup";
 import Notiflix from "notiflix";
 
+let startAmount = 40;
+
 const refs = {
    serchFrom: document.querySelector('.search-form'),
    hitsGalerry: document.querySelector('.gallery'),
    loadMoreBtn: document.querySelector('.load-more'),
-}
+};
 
 const seacrchApiService = new SeacrchApiService();
 
 refs.serchFrom.addEventListener('submit', onSearch);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
+refs.loadMoreBtn.classList.add('visually-hidden');
+
 function onSearch(e){
    e.preventDefault();
 
-   
-   seacrchApiService.searchQuery = e.currentTarget.elements.searchQuery.value;
-   
-   if(seacrchApiService.searchQuery ===''){
-      Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-   }
+   refs.loadMoreBtn.classList.add('visually-hidden');
+ 
+   seacrchApiService.query = e.currentTarget.elements.searchQuery.value.trim();
+
+   if(!seacrchApiService.query){
+      return Notiflix.Notify.failure('Your search bar is empty, please write valid value');
+   } 
 
    seacrchApiService.resetPage();
-   seacrchApiService.fetchHits().then( hits =>{
-      clearHitsGallery()
-      appendHitsMarcup(hits)});
+   seacrchApiService.fetchHits().then(hits =>{
+      if(hits.length === 0){
+         refs.loadMoreBtn.classList.add('visually-hidden');
+         return Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+      }
+      clearHitsGallery();  
+      appendHitsMarcup(hits);
+      refs.loadMoreBtn.classList.remove('visually-hidden');
+
+      if(seacrchApiService.page === 2) {
+         Notiflix.Notify.success(`Hooray! We found ${seacrchApiService.totalHits} images.`);
+      }
+   });
 }
 
 function onLoadMore() {
-   seacrchApiService.fetchHits().then(appendHitsMarcup);
+   seacrchApiService.fetchHits().then(hits =>{
+      appendHitsMarcup(hits);
+
+      startAmount += hits.length;
+   });
 }
 
 function appendHitsMarcup(hits) {
